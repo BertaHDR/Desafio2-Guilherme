41dad9189bb8d384cadde834334c3315fc4c0831
BertaHdr <gui.m.berta16@gmail.com>
2021-09-09 23:43:03 -0300

    implementando darkmode(deu erro)

--- darkmode.js
+++ darkmode.js
@@ -1,11 +1,29 @@
 const html = document.querySelector("html");
 const checkbox = document.querySelector("input[name=tema]");
 
+const coresIniciais = {
+    background: window.getComputedStyle(html).getPropertyValue("--background"),
+    backgroundPainel: window.getComputedStyle(html).getPropertyValue("--background-painel"),
+    corTitulo: window.getComputedStyle(html).getPropertyValue("--cor-titulo"),
+    corTexto: window.getComputedStyle(html).getPropertyValue("--cor-texto"),
+}
+
+const darkMode = {
+    background: "#333333",
+    backgroundPainel: "#434343",
+    corTitulo: "#cacaca",
+    corTexto: "#cacaca",
+}
+
+const transformKey = key => "--" + key.replace(/[(A-Z)]/, "-p").toLowerCase()
+
 const changeColors = (colors) => {
-    
+    Object.keys(colors).map(key => 
+        html.style.setProperty(transformKey(key), colors[key])   
+    )
 }
 
 
 checkbox.addEventListener("change", ({target}) => {
-    target.checked ? changeColors() : changeColors()
+    target.checked ? changeColors(darkMode) : changeColors(coresIniciais)
 })
