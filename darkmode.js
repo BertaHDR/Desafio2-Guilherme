1555a710a92cc2c064be8deef95f0245cdebe45f
BertaHdr <gui.m.berta16@gmail.com>
2021-09-09 22:57:40 -0300

    criando constantes para darkmode

--- darkmode.js
+++ darkmode.js
@@ -1,1 +1,11 @@
-const html = document.querySelector("html")
+const html = document.querySelector("html");
+const checkbox = document.querySelector("input[name=tema]");
+
+const changeColors = (colors) => {
+    
+}
+
+
+checkbox.addEventListener("change", ({target}) => {
+    target.checked ? changeColors() : changeColors()
+})
